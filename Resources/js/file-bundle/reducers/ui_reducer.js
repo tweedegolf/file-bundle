2f266940e21e8eec3026c3b871b424c96255738e
Daniella Cocco <daniella810@gmail.com>
2016-08-31 09:17:48 +0200

    Removed unused ui select code

--- Resources/js/file-bundle/reducers/ui_reducer.js
+++ Resources/js/file-bundle/reducers/ui_reducer.js
@@ -13,32 +13,7 @@ export function ui(state = uiInitialState, action){
 
   switch (action.type) {
 
-    case ActionTypes.SELECT_FILE:
-
-      console.log("SELECT FILE ACTION")
-      console.log("state:", state)
-      console.log("payload:", action.payload)
-
-      // Get currently selected items from state
-      let index = state.selected.findIndex(f => {
-        console.log(f.id === id)
-        return f.id === id
-      })
-
-      // If already selected,
-
-        // Deselect (remove from "selected")
-
-      // If not yet selected,
-
-        // Select (add to "selected")
-
-      // Return new state
-
-      return {
-        ...state,
-        selected: selected,
-      }
+    // cases
 
     default:
       return state
