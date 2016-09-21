c891c75f6cbe619f1eaf7c6242052f5f08b680fd
abudaan <daniel@abumarkub.net>
2016-09-21 14:33:56 +0200

    setup mocha and chai testing

--- Resources/js/test/reducers/ui_reducer_spec.js
+++ Resources/js/test/reducers/ui_reducer_spec.js
@@ -1,3 +1,19 @@
-import { expect } from 'chai';
-import { uiInitialState } from '../../file-bundle/reducers/ui_reducer';
-import { ui } from '../../file-bundle/reducers/ui_reducer';
+import {describe, it} from 'mocha';
+import chai, {assert, expect} from 'chai';
+import {uiInitialState} from '../../file-bundle/reducers/ui_reducer';
+import {ui} from '../../file-bundle/reducers/ui_reducer';
+
+chai.should();
+
+describe('uiInitialState', () => {
+  describe('#selected', () => {
+    it('should be an array', () => {
+      uiInitialState.selected.should.be.a('Array');
+      // assert.typeof(uiInitialState.selected, 'Array');
+    });
+    it('should be an empty array', () => {
+      uiInitialState.selected.should.have.length(0);
+      // assert.lengthOf(uiInitialState.selected, 0);
+    });
+  });
+});
