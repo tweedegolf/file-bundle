4718f564e81a349a7fd92ecfebf3662481948397
Daniella Cocco <daniella810@gmail.com>
2016-10-12 11:32:26 +0200

    Disabled API test

--- Resources/js/file-bundle/test/api.js
+++ Resources/js/file-bundle/test/api.js
@@ -3,23 +3,23 @@ import {describe, it} from 'mocha'
 // import assert from 'assert'
 import api from '../api'
 
-describe('api', done => {
-  describe('#openFolder', done => {
-    it('should return a folder and a files array', done => {
-
-      api.openFolder(
-        null,
-        (folders, files) => {
-          //console.log(folders, files)
-          assert.typeOf(files, 'Array', 'no folders array')
-          assert.typeOf(folders, 'Array', 'no files array')
-          done()
-        },
-        err => {
-          // console.log(err)
-          done(err)
-        }
-      )
-    })
-  })
-})
+// describe('api', done => {
+//   describe('#openFolder', done => {
+//     it('should return a folder and a files array', done => {
+//
+//       api.openFolder(
+//         null,
+//         (folders, files) => {
+//           //console.log(folders, files)
+//           assert.typeOf(files, 'Array', 'no folders array')
+//           assert.typeOf(folders, 'Array', 'no files array')
+//           done()
+//         },
+//         err => {
+//           // console.log(err)
+//           done(err)
+//         }
+//       )
+//     })
+//   })
+// })
