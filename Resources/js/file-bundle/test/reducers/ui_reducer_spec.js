525f5fa8bdafa9b5b2f9550b2d075dd5c69342dd
Daniella Cocco <daniella810@gmail.com>
2016-10-12 10:08:49 +0200

    Add folder test wip

--- Resources/js/file-bundle/test/reducers/ui_reducer_spec.js
+++ Resources/js/file-bundle/test/reducers/ui_reducer_spec.js
@@ -2,6 +2,7 @@ import {assert, expect} from 'chai'
 import {describe, it} from 'mocha'
 import {uiInitialState} from '../../reducers/ui_reducer'
 import {ui} from '../../reducers/ui_reducer'
+import * as actions from '../../actions.js'
 
 describe('uiInitialState', () => {
   describe('#selected', () => {
@@ -10,6 +11,27 @@ describe('uiInitialState', () => {
     })
     it('should be an empty array', () => {
       assert.lengthOf(uiInitialState.selected, 0)
+    })
+  })
+})
+
+describe('ui updates', () => {
+
+  describe('add folder', () => {
+
+    it('shows a progress indicator by setting adding_folder', () => {
+
+      console.log(actions)
+      const id = 2
+      const action = actions.openFolder(id)
+      const next_result = ui(uiInitialState, action)
+      console.log("")
+
+      expect(next_result).to.equal({
+        ...state,
+        loading_folder: id,
+        confirm_delete: null
+      })
     })
   })
 })
