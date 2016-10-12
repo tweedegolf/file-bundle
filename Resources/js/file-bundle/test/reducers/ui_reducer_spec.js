4c50b428c59dd8578e900d5d3e17cf349d0ca8f5
Daniella Cocco <daniella810@gmail.com>
2016-10-12 11:21:49 +0200

    Test initial state

--- Resources/js/file-bundle/test/reducers/ui_reducer_spec.js
+++ Resources/js/file-bundle/test/reducers/ui_reducer_spec.js
@@ -17,6 +17,29 @@ describe('uiInitialState', () => {
 
 describe('ui reducer', () => {
 
+  describe('defaults', () => {
+
+    it('should set a correct initial state if none is defined', () => {
+      expect(reducer(undefined, {})).to.deep.equal({
+        sort: 'create_ts',
+        ascending: false,
+        expanded: false,
+        preview: null,
+        confirm_delete: null,
+        hover: -1,
+        errors: [],
+        loading_folder: null,
+        deleting_file: null,
+        deleting_folder: null,
+        adding_folder: false,
+        uploading_files: false,
+        scroll_position: null,
+        selected: [],
+        clipboard: [],
+      })
+    })
+  })
+
   describe('open folder', () => {
 
     it('should indicate a folder is loading', () => {
