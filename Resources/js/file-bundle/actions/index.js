9f56153a50f495f1b3ed06f981dc769ed2e38a13
abudaan <daniel@abumarkub.net>
2017-03-17 21:21:14 +0100

    eslint airbnb

--- Resources/js/file-bundle/actions/index.js
+++ Resources/js/file-bundle/actions/index.js
@@ -23,11 +23,11 @@ const dispatch = store.dispatch
  *
  * @param {SelectFileArg} data Argument passed.
  */
-const selectFile = function(data){
-  dispatch({
-    type: ActionTypes.SELECT_FILE,
-    payload: {...data}
-  })
+const selectFile = function(data) {
+    dispatch({
+        type: ActionTypes.SELECT_FILE,
+        payload: {...data }
+    })
 }
 
 
@@ -41,17 +41,17 @@ const selectFile = function(data){
  *                                 array of file ids to the HTML element's
  *                                 dataset; this array is passed as argument.
  */
-const init = function(selected = null){
-  if(selected !== null){
-    dispatch({
-      type: ActionTypes.SELECT_FILES,
-      payload: {
-        selected
-      }
-    })
-  }
-  let currentFolderId = cache.init()
-  openFolder(currentFolderId)
+const init = function(selected = null) {
+    if (selected !== null) {
+        dispatch({
+            type: ActionTypes.SELECT_FILES,
+            payload: {
+                selected
+            }
+        })
+    }
+    let currentFolderId = cache.init()
+    openFolder(currentFolderId)
 }
 
 
@@ -63,27 +63,27 @@ const init = function(selected = null){
  *                                id null
  * @return     {void}  dispatches actions
  */
-const openFolder = function(id){
-  dispatch({
-    type: ActionTypes.OPEN_FOLDER,
-    payload: {id}
-  })
+const openFolder = function(id) {
+    dispatch({
+        type: ActionTypes.OPEN_FOLDER,
+        payload: { id }
+    })
 
-  cache.loadFolder(id)
-  .then(
-    payload => {
-      dispatch({
-        type: ActionTypes.FOLDER_OPENED,
-        payload,
-      })
-    },
-    payload => {
-      dispatch({
-        type: ActionTypes.ERROR_OPENING_FOLDER,
-        payload
-      })
-    }
-  )
+    cache.loadFolder(id)
+        .then(
+            payload => {
+                dispatch({
+                    type: ActionTypes.FOLDER_OPENED,
+                    payload,
+                })
+            },
+            payload => {
+                dispatch({
+                    type: ActionTypes.ERROR_OPENING_FOLDER,
+                    payload
+                })
+            }
+        )
 }
 
 
@@ -96,27 +96,27 @@ const openFolder = function(id){
  *                                           the file that will be deleted.
  * @return     {void}  dispatches actions
  */
-const deleteFile = function(file_id, current_folder_id){
-  dispatch({
-    type: ActionTypes.DELETE_FILE,
-    payload: {file_id}
-  })
+const deleteFile = function(file_id, current_folder_id) {
+    dispatch({
+        type: ActionTypes.DELETE_FILE,
+        payload: { file_id }
+    })
 
-  cache.deleteFile(file_id, current_folder_id)
-  .then(
-    payload => {
-      dispatch({
-        type: ActionTypes.FILE_DELETED,
-        payload,
-      })
-    },
-    payload => {
-      dispatch({
-        type: ActionTypes.ERROR_DELETING_FILE,
-        payload
-      })
-    }
-  )
+    cache.deleteFile(file_id, current_folder_id)
+        .then(
+            payload => {
+                dispatch({
+                    type: ActionTypes.FILE_DELETED,
+                    payload,
+                })
+            },
+            payload => {
+                dispatch({
+                    type: ActionTypes.ERROR_DELETING_FILE,
+                    payload
+                })
+            }
+        )
 }
 
 
@@ -130,38 +130,38 @@ const deleteFile = function(file_id, current_folder_id){
  *                                          will be deleted
  * @return     {void}  dispatches actions
  */
-const deleteFolder = function(folder_id, current_folder_id){
-  dispatch({
-    type: ActionTypes.DELETE_FOLDER,
-    payload: {folder_id}
-  })
+const deleteFolder = function(folder_id, current_folder_id) {
+    dispatch({
+        type: ActionTypes.DELETE_FOLDER,
+        payload: { folder_id }
+    })
 
 
-  cache.deleteFolder(folder_id, current_folder_id)
-  .then(
-    payload => {
-      dispatch({
-        type: ActionTypes.FOLDER_DELETED,
-        payload,
-      })
-    },
-    payload => {
-      dispatch({
-        type: ActionTypes.ERROR_DELETING_FOLDER,
-        payload
-      })
-    }
-  )
+    cache.deleteFolder(folder_id, current_folder_id)
+        .then(
+            payload => {
+                dispatch({
+                    type: ActionTypes.FOLDER_DELETED,
+                    payload,
+                })
+            },
+            payload => {
+                dispatch({
+                    type: ActionTypes.ERROR_DELETING_FOLDER,
+                    payload
+                })
+            }
+        )
 }
 
 
 /**
  * Cut files, i.e. move the currently selected files to the clipboard
  */
-const cutFiles = function(){
-  dispatch({
-    type: ActionTypes.CUT_FILES,
-  })
+const cutFiles = function() {
+    dispatch({
+        type: ActionTypes.CUT_FILES,
+    })
 }
 
 
@@ -177,34 +177,34 @@ const cutFiles = function(){
  *                                           where the files will be moved to.
  * @return     {void}  dispatches actions
  */
-const pasteFiles = function(files, current_folder_id){
-  // dispatch ui state action here?
+const pasteFiles = function(files, current_folder_id) {
+    // dispatch ui state action here?
 
-  cache.moveFiles(files, current_folder_id)
-  .then(
-    payload => {
-      dispatch({
-        type: ActionTypes.FILES_MOVED,
-        payload,
-      })
-    },
-    payload => {
-      dispatch({
-        type: ActionTypes.ERROR_MOVING_FILES,
-        payload
-      })
-    }
-  )
+    cache.moveFiles(files, current_folder_id)
+        .then(
+            payload => {
+                dispatch({
+                    type: ActionTypes.FILES_MOVED,
+                    payload,
+                })
+            },
+            payload => {
+                dispatch({
+                    type: ActionTypes.ERROR_MOVING_FILES,
+                    payload
+                })
+            }
+        )
 }
 
 
 /**
  * Move files in clipboard array back to the selected array
  */
-const cancelCutAndPasteFiles = function(){
-  dispatch({
-    type: ActionTypes.CANCEL_CUT_AND_PASTE_FILES,
-  })
+const cancelCutAndPasteFiles = function() {
+    dispatch({
+        type: ActionTypes.CANCEL_CUT_AND_PASTE_FILES,
+    })
 }
 
 
@@ -219,28 +219,28 @@ const cancelCutAndPasteFiles = function(){
  *                                        uploaded
  * @return     {void}  returns nothing
  */
-const upload = function(file_list, current_folder){
-  file_list = Array.from(file_list)
-  dispatch({
-    type: ActionTypes.UPLOAD_START,
-    payload: {file_list}
-  })
+const upload = function(file_list, current_folder) {
+    file_list = Array.from(file_list)
+    dispatch({
+        type: ActionTypes.UPLOAD_START,
+        payload: { file_list }
+    })
 
-  cache.addFiles(file_list, current_folder)
-  .then(
-    payload => {
-      dispatch({
-        type: ActionTypes.UPLOAD_DONE,
-        payload,
-      })
-    },
-    payload => {
-      dispatch({
-        type: ActionTypes.ERROR_UPLOADING_FILE,
-        payload
-      })
-    }
-  )
+    cache.addFiles(file_list, current_folder)
+        .then(
+            payload => {
+                dispatch({
+                    type: ActionTypes.UPLOAD_DONE,
+                    payload,
+                })
+            },
+            payload => {
+                dispatch({
+                    type: ActionTypes.ERROR_UPLOADING_FILE,
+                    payload
+                })
+            }
+        )
 }
 
 
@@ -253,26 +253,26 @@ const upload = function(file_list, current_folder){
  *                                           the new folder
  * @return     {void}  returns nothing, dispatches actions
  */
-const addFolder = function(folder_name, current_folder_id){
-  dispatch({
-    type: ActionTypes.ADD_FOLDER,
-  })
+const addFolder = function(folder_name, current_folder_id) {
+    dispatch({
+        type: ActionTypes.ADD_FOLDER,
+    })
 
-  cache.addFolder(folder_name, current_folder_id)
-  .then(
-    payload => {
-      dispatch({
-        type: ActionTypes.FOLDER_ADDED,
-        payload,
-      })
-    },
-    payload => {
-      dispatch({
-        type: ActionTypes.ERROR_ADDING_FOLDER,
-        payload
-      })
-    }
-  )
+    cache.addFolder(folder_name, current_folder_id)
+        .then(
+            payload => {
+                dispatch({
+                    type: ActionTypes.FOLDER_ADDED,
+                    payload,
+                })
+            },
+            payload => {
+                dispatch({
+                    type: ActionTypes.ERROR_ADDING_FOLDER,
+                    payload
+                })
+            }
+        )
 }
 
 
@@ -283,11 +283,11 @@ const addFolder = function(folder_name, current_folder_id){
  *                               current sorting column, the sorting order will
  *                               be reversed.
  */
-const changeSorting = function(sort){
-  dispatch({
-    type: ActionTypes.CHANGE_SORTING,
-    payload: {sort},
-  })
+const changeSorting = function(sort) {
+    dispatch({
+        type: ActionTypes.CHANGE_SORTING,
+        payload: { sort },
+    })
 }
 
 
@@ -296,11 +296,11 @@ const changeSorting = function(sort){
  *
  * @param      {number}  error_id  The unique identifier of the error.
  */
-const dismissError = function(error_id){
-  dispatch({
-    type: ActionTypes.DISMISS_ERROR,
-    payload: {error_id},
-  })
+const dismissError = function(error_id) {
+    dispatch({
+        type: ActionTypes.DISMISS_ERROR,
+        payload: { error_id },
+    })
 }
 
 
@@ -309,11 +309,11 @@ const dismissError = function(error_id){
  *
  * @param      {string}  image_url  The url of the full size image.
  */
-const showPreview = function(image_url){
-  dispatch({
-    type: ActionTypes.SHOW_PREVIEW,
-    payload: {image_url},
-  })
+const showPreview = function(image_url) {
+    dispatch({
+        type: ActionTypes.SHOW_PREVIEW,
+        payload: { image_url },
+    })
 }
 
 
@@ -324,11 +324,11 @@ const showPreview = function(image_url){
  * @param      {number}  id      The id of the file will be deleted after
  *                               confirmation
  */
-const confirmDelete = function(id){
-  dispatch({
-    type: ActionTypes.CONFIRM_DELETE,
-    payload: {id},
-  })
+const confirmDelete = function(id) {
+    dispatch({
+        type: ActionTypes.CONFIRM_DELETE,
+        payload: { id },
+    })
 }
 
 
@@ -348,14 +348,14 @@ const confirmDelete = function(id){
  *                                   the selection will wrap around resulting in
  *                                   the first item in the list being selected.
  */
-const setHover = function(diff, folder_id){
-  dispatch({
-    type: ActionTypes.SET_HOVER,
-    payload: {
-      diff,
-      max: cache.getItemCount(folder_id)
-    }
-  })
+const setHover = function(diff, folder_id) {
+    dispatch({
+        type: ActionTypes.SET_HOVER,
+        payload: {
+            diff,
+            max: cache.getItemCount(folder_id)
+        }
+    })
 }
 
 
@@ -365,11 +365,11 @@ const setHover = function(diff, folder_id){
  * @param      {number}  scroll  The position of the list in pixels measured
  *                               from the top.
  */
-const setScrollPosition = function(scroll){
-  dispatch({
-    type: ActionTypes.SET_SCROLL_POSITION,
-    payload: {scroll}
-  })
+const setScrollPosition = function(scroll) {
+    dispatch({
+        type: ActionTypes.SET_SCROLL_POSITION,
+        payload: { scroll }
+    })
 }
 
 
@@ -377,10 +377,10 @@ const setScrollPosition = function(scroll){
  * In Filepicker mode the user sees a button to expand and collapse the browser.
  * In Browser mode the browser is always expanded.
  */
-const expandBrowser = function(){
-  dispatch({
-    type: ActionTypes.EXPAND_BROWSER,
-  })
+const expandBrowser = function() {
+    dispatch({
+        type: ActionTypes.EXPAND_BROWSER,
+    })
 }
 
 
@@ -389,21 +389,21 @@ const expandBrowser = function(){
  * you want when you import it.
  */
 export default {
-  upload,
-  deleteFile,
-  openFolder,
-  addFolder,
-  deleteFolder,
-  selectFile,
-  cutFiles,
-  pasteFiles,
-  cancelCutAndPasteFiles,
-  init,
-  changeSorting,
-  dismissError,
-  showPreview,
-  confirmDelete,
-  expandBrowser,
-  setHover,
-  setScrollPosition,
+    upload,
+    deleteFile,
+    openFolder,
+    addFolder,
+    deleteFolder,
+    selectFile,
+    cutFiles,
+    pasteFiles,
+    cancelCutAndPasteFiles,
+    init,
+    changeSorting,
+    dismissError,
+    showPreview,
+    confirmDelete,
+    expandBrowser,
+    setHover,
+    setScrollPosition,
 }
