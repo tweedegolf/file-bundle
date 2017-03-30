c28ed69d63d905fbb26c033576cdf5743ada150b
abudaan <daniel@abumarkub.net>
2017-03-30 22:32:34 +0200

    recursive find folder by id

--- Resources/js/src/util/traverse.js
+++ Resources/js/src/util/traverse.js
@@ -8,10 +8,8 @@ const data = {
             folders: [{
                 id: 11,
                 name: 'konijn',
-                create_ts: 143239123123123123,
                 folders: [{
                     name: 'aap',
-                    create_ts: 1231239123123123123,
                     id: 111,
                     folders: [{
                         id: 1111,
@@ -20,16 +18,39 @@ const data = {
             }, {
                 id: 12,
                 name: 'mus',
-                create_ts: 243239123123123123,
                 folders: [{
                     name: 'winterkoninkje',
-                    create_ts: 1231239123123123123,
                     id: 121,
+                }],
+            }, {
+                id: 13,
+                name: 'aap',
+                folders: [{
+                    name: 'hond',
+                    id: 131,
+                }, {
+                    name: 'kip',
+                    id: 132,
+                }],
+            }],
+        }, {
+            id: 2,
+            folders: [{
+                id: 21,
+                name: 'haan',
+                folders: [{
+                    name: 'aap',
+                    id: 211,
+                    folders: [{
+                        id: 2111,
+                    }],
                 }],
             }],
         }],
     },
 };
+
+const getIds = (array) => R.map(item => item.id, array);
 
 const isNotLast = (max, acc, i) => {
     console.log('isNotLast', max, i);
@@ -37,6 +58,100 @@ const isNotLast = (max, acc, i) => {
 };
 const isNotFound = (test, acc, folder) => test !== folder.id;
 
+const findInArray = (id, array) => R.filter(item => item.id === id, array);
+
+const find = (folder, id, level) => {
+    // console.log(folder.folders);
+    if (folder.id === id) {
+        return [true];
+    } else if (R.isNil(folder.folders) === false) {
+        level += 1;
+        const t = R.map(f => find(f, id, level), folder.folders);
+        console.log(level, folder.id, getIds(folder.folders), t);
+        return t;
+    }
+    return [false];
+};
+const n = null;
+// const r = find(data[n], 3, 0);
+// const r = findInArray(3, data.null.folders);
+
+const check = (item, index, lookFor, parentId, level) => {
+    const c = item.id === lookFor;
+    if (R.isNil(item.folders) === false) {
+        level += 1;
+        const r = loop(item.folders, lookFor, item.id, level);
+        // console.log(item.id, R.flatten(r))
+        return [{
+            level,
+            index,
+            parent: parentId,
+            id: item.id,
+            found: c,
+        }, ...r];
+    }
+    return [{
+        level,
+        index,
+        parent: parentId,
+        id: item.id,
+        found: c,
+    }];
+};
+
+const mapIndexed = R.addIndex(R.map);
+
+const loop = (items, lookFor, parentId, level) => {
+    // console.log(id, level);
+    return mapIndexed((item, index) => check(item, index, lookFor, parentId, level), items);
+};
+const parentId = null;
+const lookFor = 132;
+const level = 0;
+const tmp = loop(data.null.folders, lookFor, parentId, level);
+const result = R.flatten(tmp);
+
+const filterFound = R.filter(o => o.found, R.flatten(result));
+console.log(filterFound);
+const found = R.length(filterFound) !== 0 ? filterFound[0] : null;
+console.log(found);
+
+
+const getParent = (items, node) => {
+    const parentId = node.parent;
+    const filtered = R.filter((item) => {
+        return item.id === parentId;
+    }, items);
+    const parent = R.length(filtered) !== 0 ? filtered[0] : null;
+    // console.log(parentId, parent);
+    return parent;
+};
+
+const getPath = (items, target) => {
+    const path = [target.index];
+    let parent = getParent(items, target);
+    if (parent === null) {
+        return path;
+    }
+    path.push(parent.index);
+    let level = parent.level;
+    while(level > 1) {
+        parent = getParent(items, parent);
+        path.push(parent.index);
+        level = parent.level;
+    }
+    const reverse = R.reverse(path);
+    return R.reduce((acc, item) => [...acc, 'folders', item], [null], reverse);
+};
+
+const p = getPath(result, found);
+const lensPath = R.lensPath(p);
+console.log(p);
+console.log(R.view(lensPath, data));
+
+export default 42;
+
+/*
 const traverseTree = (folder, id, accum) => {
     // if (R.isNil(folder.folders)) {
     //     // const isNotLastCurried = R.curry(isNotLast)(accum.depth * 2);
@@ -100,3 +215,4 @@ const path = findInTree(data, 3, rootFolderId);
 
 // console.log('path', path);
 // console.log('found', R.view(lensPath, data));
+*/
