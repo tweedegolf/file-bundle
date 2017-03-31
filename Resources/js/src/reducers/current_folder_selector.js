52bf1a76498f25e71ccba22b87f6ea5232b16b57
abudaan <daniel@abumarkub.net>
2017-04-01 00:21:05 +0200

    add selector

---
+++ Resources/js/src/reducers/current_folder_selector.js
@@ -0,0 +1,30 @@
+import R from 'ramda';
+import { createSelector } from 'reselect';
+
+const getUI = state => state.ui;
+const getTree = state => state.tree;
+
+const resetNew = array => R.map(f => ({ ...f, new: false }), array);
+const filterTrashed = array => R.filter(f => f.isTrashed !== true, array);
+const sortAscendingBy = (key, array) => R.sort(R.ascend(R.prop(key)), array);
+const sortDescendingBy = (key, array) => R.sort(R.descend(R.prop(key)), array);
+
+export default createSelector(
+    [getUI, getTree],
+    (ui, tree) => {
+        const {
+            sort,
+            ascending,
+        } = ui;
+
+        const sortBy = ascending ? sortAscendingBy : sortDescendingBy;
+        const currentFolder = tree.currentFolder;
+        const files = R.compose(R.curry(sortBy)(sort), filterTrashed)(currentFolder.files);
+        const folders = sortBy(sort, currentFolder.folders);
+
+        return {
+            files,
+            folders,
+        };
+    },
+);
